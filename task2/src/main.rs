a2d0e47621872a5ae63d7177d5f33a8121b37c3a
0xAct0r <gizatullininsaf25@gmail.com>
2023-05-22 09:09:57 +0300

    bruh

--- task2/src/main.rs
+++ task2/src/main.rs
@@ -4,16 +4,19 @@ use std::{fs::File, io::Read, str::Chars};
 // A X 3 + 1 = 4, 3 for draw and 1 for X or ROCK
 // A Y 6 + 2 = 8, 6 for win and 2 for Paper
 // A Z 3 + 0 = 3, 0 for lost and 3 for Scissors
-
+// and also for B and C
 
 fn main() {
     let mut h = HashMap::new();
-    h.insert("A", 1);
-    h.insert("B", 2);
-    h.insert("C", 3);
-    h.insert("X", 1);
-    h.insert("Y", 2);
-    h.insert("Z", 3);
+    h.insert(["A", "X"], 4);
+    h.insert(["A", "Y"], 8);
+    h.insert(["A", "Z"], 3);
+    h.insert(["B", "X"], 1);
+    h.insert(["B", "Y"], 5);
+    h.insert(["B", "Z"], 9);
+    h.insert(["C", "X"], 7);
+    h.insert(["C", "Y"], 2);
+    h.insert(["C", "Z"], 6);
     
     let mut s = String::new();
     let mut f = File::open("inp.txt").unwrap();
