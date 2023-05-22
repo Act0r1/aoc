1026ce1e57da363312f93fea776d1b2ef5035017
0xAct0r <gizatullininsaf25@gmail.com>
2023-05-22 10:00:28 +0300

    done day2

--- task2/src/main.rs
+++ task2/src/main.rs
@@ -1,22 +1,22 @@
 use std::collections::HashMap;
 use std::{fs::File, io::Read, str::Chars};
 // there a 9 ways to play this game like:
-// A X 3 + 1 = 4, 3 for draw and 1 for X or ROCK
-// A Y 6 + 2 = 8, 6 for win and 2 for Paper
-// A Z 3 + 0 = 3, 0 for lost and 3 for Scissors
+// A X 3 + 0 = 0, 3 for Scissors
+// A Y 3 + 1 = 4,  3 for draw and 1 for Rock
+// A Z 6 + 2 = 8, 6 for win and 2 for Papers 
 // and also for B and C
 
 fn main() {
     let mut h = HashMap::new();
-    h.insert(["A", "X"], 4);
-    h.insert(["A", "Y"], 8);
-    h.insert(["A", "Z"], 3);
+    h.insert(["A", "X"], 3);
+    h.insert(["A", "Y"], 4);
+    h.insert(["A", "Z"], 8);
     h.insert(["B", "X"], 1);
     h.insert(["B", "Y"], 5);
     h.insert(["B", "Z"], 9);
-    h.insert(["C", "X"], 7);
-    h.insert(["C", "Y"], 2);
-    h.insert(["C", "Z"], 6);
+    h.insert(["C", "X"], 2);
+    h.insert(["C", "Y"], 6);
+    h.insert(["C", "Z"], 7);
 
     let mut s = String::new();
     let mut f = File::open("inp.txt").unwrap();
