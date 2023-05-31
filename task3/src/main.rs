07611d447ae23c976d24abad6e5f091faa648dde
0xAct0r <gizatullininsaf25@gmail.com>
2023-06-01 00:27:51 +0300

    finish task3

--- task3/src/main.rs
+++ task3/src/main.rs
@@ -16,30 +16,53 @@ fn main() {
         .zip(27..=52)
         .collect();
     let mut sum = 0;
-    for x in s.lines() {
-        let line = x.to_string();
-        // println!("{:?}", line.split_at(x.len() / 2));
-        let (first, second) = line.split_at(x.len() / 2);
-        let first_hash: Vec<char> = first.chars().into_iter().collect();
-        let second_hash: Vec<char> = second.chars().into_iter().collect();
-        let both_contains_char = first_hash.iter().find(|c| second_hash.contains(c)).unwrap();
-        match both_contains_char.is_uppercase() {
+    let v: Vec<_> = s.lines().into_iter().collect();
+    let mut res = v.chunks(3);
+    while let Some(s) = res.next() {
+        let first_hash: Vec<char> = s.first().unwrap().chars().into_iter().collect();
+        let second_hash: Vec<char> = s[1].chars().into_iter().collect();
+        let third_hash: Vec<char> = s.last().unwrap().chars().into_iter().collect();
+        let contains_char = first_hash
+            .iter()
+            .find(|c| second_hash.contains(c) && third_hash.contains(c))
+            .unwrap();
+        match contains_char.is_uppercase() {
             true => {
-                let (_, num) = upper_case
-                    .iter()
-                    .find(|(c, _)| c == both_contains_char)
-                    .unwrap();
+                let (_, num) = upper_case.iter().find(|(c, _)| c == contains_char).unwrap();
                 sum += num;
             }
             false => {
-                let (_, num) = lower_case
-                    .iter()
-                    .find(|(c, _)| c == both_contains_char)
-                    .unwrap();
+                let (_, num) = lower_case.iter().find(|(c, _)| c == contains_char).unwrap();
                 sum += num;
-
             }
         }
     }
     println!("{:?}", sum);
+
+    // for x in s.lines().collect {
+    //     let line = x.to_string();
+    //     // println!("{:?}", line.split_at(x.len() / 2));
+    //     let (first, second) = line.split_at(x.len() / 2);
+    //     let first_hash: Vec<char> = first.chars().into_iter().collect();
+    //     let second_hash: Vec<char> = second.chars().into_iter().collect();
+    //     let both_contains_char = first_hash.iter().find(|c| second_hash.contains(c) && !second_hash.contains(c)).unwrap();
+    //     match both_contains_char.is_uppercase() {
+    //         true => {
+    //             let (_, num) = upper_case
+    //                 .iter()
+    //                 .find(|(c, _)| c == both_contains_char)
+    //                 .unwrap();
+    //             sum += num;
+    //         }
+    //         false => {
+    //             let (_, num) = lower_case
+    //                 .iter()
+    //                 .find(|(c, _)| c == both_contains_char)
+    //                 .unwrap();
+    //             sum += num;
+    //
+    //         }
+    //     }
+    // }
+    // println!("{:?}", sum);
 }
