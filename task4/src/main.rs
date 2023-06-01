3e100b3a5492535f9525b65e295178f2c33341f3
0xAct0r <gizatullininsaf25@gmail.com>
2023-06-01 23:01:53 +0300

    finish first part

--- task4/src/main.rs
+++ task4/src/main.rs
@@ -1,10 +1,29 @@
-use std::fs::File;
-use std::io::Read;
+use std::fs;
 fn main() {
-    let mut s = String::new();
-    let mut f = File::open("inp.txt").unwrap();
-    f.read_to_string(&mut s).unwrap();
-    let d: Vec<&str> = s.lines().into_iter().collect();
-    let first:Vec<_> = d.iter().map(|c| c.split(",").next().unwrap()).collect();
-    let second:Vec<_> = d.iter().map(|c| c.split(",").last().unwrap()).collect();
+    let s = fs::read_to_string("inp.txt").unwrap();
+    let lines: Vec<&str> = s.lines().into_iter().collect();
+    let first: Vec<_> = lines.iter().map(|c| c.split(",").next().unwrap()).collect();
+    let second: Vec<_> = lines.iter().map(|c| c.split(",").last().unwrap()).collect();
+    let mut sum = 0;
+    let len = first.len() - 1;
+    for x in 0..=len {
+        let (a, b) = parse(first[x]);
+        let (c, d) = parse(second[x]);
+        let cond1 = c >= a && b >= d || a >= c && d >= b;
+        match cond1 {
+            true => {
+                sum += 1;
+            }
+            _ => {}
+        }
+    }
+    println!("{:?}", sum);
+}
+
+fn parse(src: &str) -> (i32, i32) {
+    let all_nums: Vec<_> = src.split("-").collect();
+    (
+        all_nums[0].parse::<i32>().unwrap(),
+        all_nums[1].parse::<i32>().unwrap(),
+    )
 }
